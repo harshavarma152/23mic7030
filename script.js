8566239cc95cc284029956a49e1cd2a8bbe204be
harshavarma152 <gadirajuharshavarma@gmail.com>
2026-09-04 23:40:19 +0530

    Delete script.js

--- script.js
+++
@@ -1,3 +0,0 @@
-const menuBtn=document.querySelector('.menu-btn');const nav=document.querySelector('.navbar nav');
-menuBtn.addEventListener('click',()=>nav.classList.toggle('open'));
-document.querySelectorAll('.navbar nav a').forEach(link=>link.addEventListener('click',()=>nav.classList.remove('open')));
